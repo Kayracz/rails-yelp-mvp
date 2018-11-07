17881a78184053a4426fdba4d566a3097acbf5d5
Kayra Czerniewicz <kayracz@gmail.com>
2018-11-07 18:21:25 -0300

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category_id = %W(chinese italian japanese french belgian)
+
+5.times do
+  @restaurant = Restaurant.new(
+    name: Faker::GameOfThrones.character,
+    address: Faker::TheFreshPrinceOfBelAir.quote,
+    phone_number: Faker::IDNumber.valid,
+    category: category_id.sample(1).first)
+  @restaurant.save
+  p @restaurant
+end
